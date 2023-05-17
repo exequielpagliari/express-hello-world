6ca22a8de96010211d53d873863f99765a9c108c
Exequiel Pagliari <exepa@hotmail.com>
2023-05-17 07:10:29 -0300

    Prueba de respuesta

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => res.type('html').send(html));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
+var http = require('http');
+var server = http.createServer();
+ 
+function mensaje(petic, resp) {
+	resp.writeHead(200, {'content-type': 'text/plain'});
+	resp.write('Hola Mundo');
+	resp.end();
+}
+server.on('request', mensaje);
+ 
+server.listen(3000, function () {
+  	console.log('La Aplicación está funcionando en el puerto 3000');
+});
+
+
 const html = `
 <!DOCTYPE html>
 <html>
